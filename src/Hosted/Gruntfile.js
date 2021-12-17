2fd6272d5dd75126a1a953a5f2b6f4c3040af589
Angel D. Munoz <scythes.lol@gmail.com>
2021-12-17 09:31:01 -0600

    move to dart-sass

--- src/Hosted/Gruntfile.js
+++ src/Hosted/Gruntfile.js
@@ -1,27 +1,27 @@
 module.exports = function(grunt) {
-	const sass = require('node-sass');
-	 
+	const sass = require('sass');
+
 	grunt.initConfig({
-	    sass: {
-	        options: {
-	            implementation: sass
-	        },
-	        dist: {
-	            files: {
-	                'css/all.css': 'scss/all.scss'
-	            }
-	        }
-	    },
-	    cssmin: {
+		sass: {
+			options: {
+				implementation: sass
+			},
+			dist: {
+				files: {
+					'css/all.css': 'scss/all.scss'
+				}
+			}
+		},
+		cssmin: {
 			build: {
-			  src: 'css/all.css',
-			  dest: 'css/all.min.css'
+				src: 'css/all.css',
+				dest: 'css/all.min.css'
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	 
+
 	grunt.registerTask('default', ['sass', 'cssmin']);
 };
